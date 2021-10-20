82cb58a95218714f61ca7f7e2beaee3b8eb66ccb
djurdjacitakovic <djukacitakovic@gmail.com>
2021-10-20 14:07:39 +0200

    Route parameters

--- routes/expenseGroupRoutes.js
+++ routes/expenseGroupRoutes.js
@@ -2,20 +2,27 @@ const express = require("express");
 const app = express.Router();
 
 
-app.post('/expense-groups', function (req, res) {
+app.post('/', function (req, res) {
     res.send('Got a POST request')
   });
   
-  app.get('/expense-groups', function (req, res) {
+  app.get('/', function (req, res) {
     res.send('expense groups GET')
   });
   
-  app.put('/expense-groups', function (req, res) {
+  app.get('/:userId', function (req, res) {
+    res.send(`expense groups GET, userId:${req.params.userId}`)
+  });
+
+  app.put('/', function (req, res) {
     res.send('Got a PUT request at /expense-groups')
   });
   
+  app.put('/:userId', function (req, res) {
+    res.send(`Got a PUT request at /expense-groups, userId:${req.params.userId} `)
+  });
   
-  app.delete('/expense-groups', function (req, res) {
+  app.delete('/', function (req, res) {
     res.send('Got a DELETE request at /expense-groups')
   });
 
