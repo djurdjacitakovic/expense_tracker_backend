a5dcf4a30f9095a5f03fd00982591522f7bca414
djurdjacitakovic <djukacitakovic@gmail.com>
2021-10-20 11:54:59 +0200

    New folder Routes

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
+const port = 3000;
 const express = require("express");
 const app = express();
-const port = 3000;
+
 
 app.get('/', function (req, res) {
   res.send('hello world')
 });
 
-app.post('/expense-groups', function (req, res) {
-  res.send('Got a POST request')
-});
-
-app.get('/expense-groups', function (req, res) {
-  res.send('expense groups GET')
-});
-
-app.put('/expense-groups', function (req, res) {
-  res.send('Got a PUT request at /expense-groups')
-});
+var expenseGroupRoutes = require('./routes/expenseGroupRoutes');
 
 
-app.delete('/expense-groups', function (req, res) {
-  res.send('Got a DELETE request at /expense-groups')
-});
+app.use('/expenseGroupRoutes',expenseGroupRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
 
