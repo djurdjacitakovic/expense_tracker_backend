a043298ab809c81ee7701b87c761c228f67e8749
djurdjacitakovic <djukacitakovic@gmail.com>
2021-10-20 10:32:32 +0200

    expense groups routes

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World !')
-})
+app.get('/', function (req, res) {
+  res.send('hello world')
+});
+
+app.post('/expense-groups', function (req, res) {
+  res.send('Got a POST request')
+});
+
+app.get('/expense-groups', function (req, res) {
+  res.send('expense groups GET')
+});
+
+app.put('/expense-groups', function (req, res) {
+  res.send('Got a PUT request at /expense-groups')
+});
+
+
+app.delete('/expense-groups', function (req, res) {
+  res.send('Got a DELETE request at /expense-groups')
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+
