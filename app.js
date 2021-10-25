ad91d7d569f7edc1cb09f3a4780061b5910a4561
djurdjacitakovic <djukacitakovic@gmail.com>
2021-10-25 15:13:34 +0200

    Database crud-izmene

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const port = 3000;
 const express = require("express");
-const Router = require("./routes/expenseGroupRoutes")
-const Server = require("./server")
+const expenseGroupRouter = require("./routes/expenseGroupRoutes")
 const app = express();
+const mongoose = require("mongoose");
+const username = "Djurdja";
+const password = "asdf1234";
+const cluster = "cluster0";
+const dbname = "myFirstDatabase";
 
 app.get('/', function (req, res) {
   res.send('hello world')
@@ -10,8 +14,20 @@ app.get('/', function (req, res) {
 
 app.use(express.json());
 
-app.use(Server);
-app.use('/expense-groups',Router);
+mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}.hy0bu.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.use('/expense-groups',expenseGroupRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- routes/expenseGroupRoutes.js
+++ routes/expenseGroupRoutes.js
@@ -3,13 +3,10 @@ const express = require("express");
 const app = express.Router();
 const expenseGroupModel = require("../models");
 
-
-
 app.post('/', async (request, response) => {
-   const exp = new expenseGroupModel(request.body);
-
     try 
     {
+      const exp = new expenseGroupModel(request.body);
       await exp.save();
       response.send(exp);
 
@@ -19,14 +16,22 @@ app.post('/', async (request, response) => {
     }
 });
 
-app.put('/:Id', async(req, res) =>{
-
-  await expenseGroupModel.findByIdAndUpdate(req.params.Id,{"name":`${req.body.name}`,"description":`${req.body.description}`});
+app.put('/:Id', function(req, res) {
 
   try
   {
-     const y= await expenseGroupModel.findById(req.params.Id);
-     res.send(y);
+     expenseGroupModel.findByIdAndUpdate(req.params.Id,{"name":`${req.body.name}`,"description":`${req.body.description}`},
+      {new: true},
+       function(err,docs)
+     {
+      if (err){
+        res.send(err);
+    }
+    else{
+      res.send(docs);
+    }
+     });
+       
   }
   catch (error)
   { 
@@ -38,9 +43,9 @@ app.put('/:Id', async(req, res) =>{
 
 app.get('/', async(req, res)=>{
  
-  const exp = await expenseGroupModel.find({});
   try 
   {
+    const exp = await expenseGroupModel.find({});
     res.send(exp);
 
   } catch (error)
@@ -52,9 +57,9 @@ app.get('/', async(req, res)=>{
 
 app.get('/:Id', async (req, res) =>{
 
-  const exp = await expenseGroupModel.findById(req.params.Id);
   try 
   {
+    const exp = await expenseGroupModel.findById(req.params.Id);
     res.send(exp);
   } catch (error)
    {
@@ -65,12 +70,14 @@ app.get('/:Id', async (req, res) =>{
 
 
 app.delete('/:Id', async(req, res)=>{
-try{
+
+try
+{
  await expenseGroupModel.findByIdAndDelete(req.params.Id);
- res.send('izbrisano');
+ res.send(`The object(ID: ${req.params.Id}) was deleted.`);
 }
 catch (error)
-   {
+  {
     res.status(500).send(error);
   }
 
